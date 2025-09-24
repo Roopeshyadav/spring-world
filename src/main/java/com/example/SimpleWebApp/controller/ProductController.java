f260782194259cbd621fc90366c4510eeaca46d5
Roopesh Yadav <roopeshyadav95@gmail.com>
2025-09-24 17:17:25 +0530

    add basic security

--- src/main/java/com/example/SimpleWebApp/controller/ProductController.java
+++ src/main/java/com/example/SimpleWebApp/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.example.SimpleWebApp.controller;
 import com.example.SimpleWebApp.model.Product;
 import com.example.SimpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +18,36 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts(){
-        return productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+        var list = productService.getProducts();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/products/{productId}")
-    public Product getProduct(@PathVariable int productId){
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable int productId){
+        var product = productService.getProduct(productId);
+        if(product != null){
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/products")
-    public void createProduct(@RequestBody Product product){
+    public ResponseEntity<Void> createProduct(@RequestBody Product product){
         productService.addProduct(product);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/products")
-    public void updateProduct(@RequestBody Product product){
+    public ResponseEntity<Void> updateProduct(@RequestBody Product product){
         productService.updateProduct(product);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/products/{productId}")
-    public void deleteProduct(@PathVariable int productId){
+    public ResponseEntity<Void> deleteProduct(@PathVariable int productId){
         productService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
